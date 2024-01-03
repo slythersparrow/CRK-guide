c31d7bea90d63bb282c22ef1991cb8e4c31eb104
Christine Lee <christine.lee256@gmail.com>
2024-01-02 19:59:04 -0500

    Added a test cookie data JSON file with keys, updated jQuery script

--- scripts/cookie-profile.js
+++ scripts/cookie-profile.js
@@ -1,11 +1,18 @@
-var rawbase = 'https://raw.githubusercontent.com/';
-var jsonloc = '/slythersparrow/CRK-guide/main/scripts/json/test.json';
+// This tells which json file to use
+var url='https://raw.githubusercontent.com/slythersparrow/CRK-guide/slythersparrow-patch-1/scripts/json/test%20with%20key.json';
 
-$.getJSON(rawbase + jsonloc, function( data ) {
-    console.log(data);
-    //do what you want with data
-    const db = data;
-    document.getElementById("cookieName").innerHTML = "It worked?";
-});
-
-document.getElementById("demo2").innerHTML = db.cookie_crunchy.cookieName;
+// This tells the page to wait until jQuery has loaded, so the Ajax call can be called
+$(document).ready(function(){
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        error: function(){
+          console.log('JSON FAILED for data');
+        },
+        success:function(results){
+    /* the results is your json, you can reference the elements directly by using it here, without creating any additional variables */
+        var cartItemsList = document.getElementById("demo3");
+        cartItemsList.innerHTML = results.cookie0530.cookieName;
+      }  // end of success fn
+     }) // end of Ajax call
+   }) // end of $(document).ready() function
