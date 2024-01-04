2540a9377e497c7ee0c0cbc27fc41f17c5dec21f
Christine Lee <christine.lee256@gmail.com>
2024-01-04 18:40:27 -0500

    Started organizing profile sections

--- scripts/cookie-profile.js
+++ scripts/cookie-profile.js
@@ -1,18 +1,20 @@
 // This tells which json file to use
-var url='https://raw.githubusercontent.com/slythersparrow/CRK-guide/slythersparrow-patch-1/scripts/json/test%20with%20key.json';
-
+const url='https://raw.githubusercontent.com/slythersparrow/CRK-guide/main/scripts/json/cookie-profile-data.json';
+const url2='https://raw.githubusercontent.com/slythersparrow/CRK-guide/main/scripts/json/test.json';
 // This tells the page to wait until jQuery has loaded, so the Ajax call can be called
 $(document).ready(function(){
-    $.ajax({
-        url: url,
-        dataType: 'json',
-        error: function(){
-          console.log('JSON FAILED for data');
-        },
-        success:function(results){
+  $.ajax({
+    url: url2,
+    dataType: 'json',
+    error: function(){
+      console.log('JSON FAILED for data');
+    },
+    success:function(results){
     /* the results is your json, you can reference the elements directly by using it here, without creating any additional variables */
-        var cartItemsList = document.getElementById("demo3");
-        cartItemsList.innerHTML = results.cookie0530.cookieName;
-      }  // end of success fn
-     }) // end of Ajax call
-   }) // end of $(document).ready() function
+      let cartItemsList = document.getElementById("demo3");
+      let a = 'cookie0537';
+      //cartItemsList.innerHTML = results[a]['skillStatBase'][0];
+      cartItemsList.innerHTML = results['rarities'][0]['ancient'];
+    }  // end of success fn
+  }) // end of Ajax call
+}) // end of $(document).ready() function
